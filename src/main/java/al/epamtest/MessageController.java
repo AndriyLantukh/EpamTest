e3f2df58c43a74a323537717d788f9011df3dde5
AndriyLantukh <lantukh1975>
2017-04-15 14:13:35 +0300

    Added ordersPanel (only table)

--- src/main/java/al/epamtest/MessageController.java
+++ src/main/java/al/epamtest/MessageController.java
@@ -1,15 +1,26 @@
 package al.epamtest;
 
+import al.epamtest.al.epamtest.model.TheMessage;
 import lombok.Data;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
 
 @RestController
 public class MessageController {
 
+    Queue<Long> messageQ = new LinkedList<Long>();
+
+    Map<Long, TheMessage> messageMap = new HashMap<>();
+
+  //  int first = myQ.poll();// retrieve and remove the first element
 
 
     @RequestMapping(value = "/message", method = RequestMethod.POST, consumes = "application/json; charset=UTF-8")
@@ -19,7 +30,10 @@ public class MessageController {
         theMessage.setToList(c.getToList());
         theMessage.setBody(c.getBody());
 
-       return theMessage;
+        messageQ.add(theMessage.getId());
+        messageMap.put(theMessage.getId(), theMessage);
+
+        return theMessage;
     }
 
 }
